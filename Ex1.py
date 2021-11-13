e54985262a5c6798312a885caa00fd872f6851de
adiy55 <adiyafe55@gmail.com>
2021-11-13 16:16:54 +0200

    Fix main function

--- Ex1.py
+++ Ex1.py
@@ -6,11 +6,43 @@ from Building import Building
 from CallForElevator import CallForElevator
 from Elevator import Elevator
 
-filePath = "Ex1_input/Ex1_Buildings/B1.json"
+filePath = "Ex1_input/Ex1_Buildings/B2.json"
 b = Building(filePath)
-# e = b.get_elevators()[0]
+calls = pd.read_csv("Ex1_input/Ex1_Calls/Calls_c.csv", index_col=False, header=None)  # DataFrame of calls
+elevators = b.get_elevators()
 
-# print(e.get_speed())
+while not calls[calls[5] == -1].empty:
+    c = calls[calls[5] == -1].iloc[0]
+    call = CallForElevator(c)
+    elev_index = 0
+    min_time = Time.get_new_call_time(elevators[elev_index], call)
+    for i in range(1, len(elevators)):
+        tmp_time = Time.get_new_call_time(elevators[i], call)
+        if tmp_time < min_time:
+            min_time = tmp_time
+            elev_index = i
+    calls.loc[c.name, 5] = elev_index  # c.name is the row, 5 is the col
+    elevators[elev_index].get_call_list().append(call)
+
+    # curr_length = len(elevators[elev_index].get_call_list())
+
+    elevators[elev_index].set_time_to_finish(min_time)
+    unanswered_calls = calls[calls[5] == -1]
+    for i in range(len(unanswered_calls)):
+        curr_call = unanswered_calls.iloc[i]
+        call = CallForElevator(curr_call)
+        curr_time = Time.call_falls_in_range(elevators[elev_index], call)
+        if curr_time:
+            calls.loc[curr_call.name, 5] = elev_index
+            elevators[elev_index].set_time_to_finish(Time.time_to_stop(elevators[elev_index]))
+            elevators[elev_index].get_call_list().insert(0, call)
+
+print(len(calls[calls[5] == 0]))
+print(len(calls[calls[5] == 1]))
+
+# -----------------------------------------------------------------------------
+
+# need to add if for checking if each call can be reached (the floors are in range of building)
 
 # f = open(filePath)
 #
@@ -21,41 +53,3 @@ b = Building(filePath)
 # print(building["_elevators"][0])
 #
 # f.close()
-
-
-calls = pd.read_csv("Ex1_input/Ex1_Calls/Calls_a.csv", index_col=False, header=None)  # DataFrame of calls
-elevators = b.get_elevators()
-while not calls[calls[5] == -1].empty:
-    c = CallForElevator(calls[calls[5] == -1].iloc[0])
-    elev_idx = 0
-    min_time = Time.get_new_call_time(elevators[elev_idx], c)
-    for i in range(1, len(elevators)):
-        curr_time = Time.get_new_call_time(elevators[i], c)
-        if curr_time < min_time:
-            min_time = curr_time
-            elev_idx = i
-    curr_c = calls[calls[5] == -1].iloc[0]
-    calls.iloc[curr_c.name, 5] = elev_idx
-    calls = calls[calls[5] == -1]
-    # break
-    elevators[elev_idx].get_call_list().append(c)
-    # print(len(calls.loc[calls[5] == -1]))
-    curr_calls = calls.loc[(calls[1] <= min_time) & (calls[5] == -1)]
-    # print(len(curr_calls))
-
-    # pd.options.mode.chained_assignment = None  # default='warn'
-    for i in range(len(curr_calls)):
-        curr_c = CallForElevator(curr_calls.iloc[i])
-        curr_t = Time.call_falls_in_range(elevators[elev_idx], curr_c)
-        if curr_t:
-            calls.loc[curr_calls.iloc[i].name, 5] = elev_idx
-            elevators[elev_idx].get_call_list().append(curr_c)
-
-# print((calls.iloc[0]).to_list())
-# need to add if for checking if each call can be reached (the floors are in range of building)
-
-
-# print(calls.head())
-# calls.iloc[0, 5] = 2
-# print(calls.head())
-# print(calls[calls[5] == -1])
