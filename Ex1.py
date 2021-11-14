c3d11b02e5b32c200816d70cfa9a71f53babf1fd
adiy55 <adiyafe55@gmail.com>
2021-11-14 10:21:22 +0200

    Use copy of DataFrame to fix bug

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,3 @@
-import json
 import sys
 
 import pandas as pd
@@ -6,26 +5,20 @@ import pandas as pd
 import Time
 from Building import Building
 from CallForElevator import CallForElevator
-from Elevator import Elevator
 
-
-# filePath = "Ex1_input/Ex1_Buildings/B3.json"
-# calls = pd.read_csv("Ex1_input/Ex1_Calls/Calls_c.csv", index_col=False, header=None)  # DataFrame of calls
-
-# inp = sys.argv
-# b_path, i_path, o_path = inp[1], inp[2], inp[3]
+inp = sys.argv
+b_path, i_path, o_path = inp[1], inp[2], inp[3]
 
 
 def main(building_path, input_path, output_path):
     b = Building(building_path)
     elevators = b.get_elevators()
     df = pd.read_csv(input_path, index_col=False, header=None)  # DataFrame of calls
-    calls = df
-    # ignore rows with floors out of building floors range
-    #
-    # calls = df.loc[
-    #     ((df[2] >= b.get_min_floor()) & (df[2] <= b.get_max_floor()) & (df[3] >= b.get_min_floor()) & (
-    #             df[3] <= b.get_max_floor()))]
+
+    # copy of DataFrame with calls in building floors range
+    calls = df.loc[((df[2] >= b.get_min_floor()) & (df[2] <= b.get_max_floor()) & (df[3] >= b.get_min_floor()) & (
+            df[3] <= b.get_max_floor()))].copy()
+    print(len(calls))
 
     while not calls[calls[5] == -1].empty:
         c = calls[calls[5] == -1].iloc[0]
@@ -47,7 +40,7 @@ def main(building_path, input_path, output_path):
     unanswered_calls = calls[calls[5] == -1]
 
     for i in range(len(unanswered_calls)):
-        curr_call = unanswered_calls.iloc[i]
+        curr_call = unanswered_calls.loc[i]
         call = CallForElevator(curr_call)
         curr_time = Time.call_falls_in_range(elevators[elev_index], call)
         if curr_time and (
@@ -59,14 +52,14 @@ def main(building_path, input_path, output_path):
             elevators[elev_index].get_call_list().insert(curr_length, call)
             curr_length += 1
 
-    # print(len(calls[calls[5] == 0]))
-    # print(len(calls[calls[5] == 1]))
+    for i in range(len(elevators)):
+        print(len(calls[calls[5] == i]))
 
     df.to_csv(output_path, index=False, header=False)  # todo: how to move last line in csv
 
 
-# main(b_path, i_path, o_path)
-main("Ex1_input/Ex1_Buildings/B1.json", "Ex1_input/Ex1_Calls/Calls_a.csv", "Ex1_input/output.csv")
+main(b_path, i_path, o_path)
+# main("Ex1_input/Ex1_Buildings/B2.json", "Ex1_input/Ex1_Calls/Calls_c.csv", "Ex1_input/output.csv")
 
 # -----------------------------------------------------------------------------
 
